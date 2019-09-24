92beb9ca9f40b78f943a2e9383f1d0ba6df462b9
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-09-24 22:49:47 +0900

    finish login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    get 'sessions/login'
-    get 'sessions/logout'
+    post '/sessions/login'
+    get '/sessions/logout'
     resource :user
   end
 end
