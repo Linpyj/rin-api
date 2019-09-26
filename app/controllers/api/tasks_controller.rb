f3b720e8d78b03b08c8d058d02844fa54d1baf4b
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-09-26 16:16:18 +0900

    create task index and show adn create function

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,17 +1,29 @@
 class Api::TasksController < Api::ApplicationController
   def index
-
+    @task = Task.all
+    render json: @task
   end
 
   def show
+    @task = Task.find_by(id: params[:id])
+    render json: @task
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    render json: @task
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :detail)
   end
 end
