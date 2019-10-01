84a23895f7af2278e22c101bad38bbd926fedcc3
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-10-01 12:07:08 +0900

    create task#destroy

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -20,9 +20,14 @@ class Api::TasksController < Api::ApplicationController
   end
 
   def create
-    @task = @current_user.tasks.create(content: params[:content], detail: params[:detail], user_id: @current_user.id)
+    @task = @current_user.tasks.create(content: params[:content], detail: params[:detail])
     @task.save
     render json: @task
+  end
+
+  def destroy
+    @task = Task.find_by(id: params[:id])
+    @task.destroy
   end
 
 end
