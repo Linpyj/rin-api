2c276e6f6a922dfd0900f4994bbd35e4b369b281
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-10-03 20:33:06 +0900

    completed

--- app/controllers/api/v1/manager/application_controller.rb
+++ app/controllers/api/v1/manager/application_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Manager::ApplicationController < Api::V1::ApplicationController
   before_action :manager?
 
   def manager?
-    return render(json: { error: :unauthorized }, status: 401) unless @current_user.is_manager
+    return render(json: { error: :unauthorized }, status: 401) unless !!@current_user.is_manager
   end
 end
