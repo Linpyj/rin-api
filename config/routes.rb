41385f1bd3e738174a93d35a71e31017477a9416
藤井麟太郎 <atarimaetaisou@icloud.com>
2019-10-04 11:26:43 +0900

    delete useless array

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
-# Rails.application.routes.draw do
-#
-#   namespace :api do
-#     post '/sessions/login'
-#     get '/sessions/logout'
-#     resources :users
-#     resources :tasks
-#   end
-# end
-#
-
 Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
       post '/sessions/login'
       get '/sessions/logout'
-      resources :user
+      resources :users
       resources :tasks
       namespace :manager do
         resources :users
---
+++ vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi-x86_64-darwin18/include/ffitarget.h
@@ -0,0 +1,1 @@
+/Users/rinrin/Desktop/Workplace/Whatodo/projects/rin-api/vendor/bundle/ruby/2.6.0/gems/ffi-1.11.1/ext/ffi_c/libffi/src/x86/ffitarget.h
